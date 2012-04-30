48203e7df57d349e3d22de4036e694d775b467ab
Liam Kaufman <liam.kaufman@gmail.com>
2012-04-30 18:05:53 -0400

    added cors again

--- web/app.js
+++ web/app.js
@@ -109,6 +109,11 @@ app.configure('production', function(){
 });
 
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 
 
