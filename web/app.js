866588b0c61a46fbe3a774164a93622470610e5e
Liam Kaufman <liam.kaufman@gmail.com>
2012-04-30 18:19:21 -0400

    CORS again

--- web/app.js
+++ web/app.js
@@ -40,6 +40,8 @@ cache = {};
 
 
 function staticCaching( req, res, next ){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   var url = req.url,
       encoding, ext;
 
@@ -109,11 +111,6 @@ app.configure('production', function(){
 });
 
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
 
 
 
--- web/routes/index.js
+++ web/routes/index.js
@@ -153,6 +153,7 @@ exports.understoodit = function(req, res){
   if(options.environment === 'development'){
     options.jsIncludes = require('../public/javascripts/app.json');
   }
-
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.render('app/index', options);
 };
